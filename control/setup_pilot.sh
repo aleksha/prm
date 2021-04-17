98b826cdbd14b43d5fb2d333f8c3a2ce502730b5
Alexey Dzyuba <a.dzyuba@gmail.com>
2021-04-17 12:03:22 +0200

    add cleaning scripts

--- control/setup_pilot.sh
+++ control/setup_pilot.sh
@@ -6,5 +6,7 @@ cp -r -v ../code/g4app/pilot source
 cd source;
 cp -v ../../configs/geom/amber-pilot/geom_config.h ./;
 cd ../../;
+echo "Make output/ directory";
+mkdir output;
 echo "Setup is done. You can try to build app.";
 echo "";
