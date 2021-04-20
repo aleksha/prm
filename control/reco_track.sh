bfd8f0480f521dc3fc3f880cd0f8c2c132bab740
Alexey Dzyuba <a.dzyuba@gmail.com>
2021-04-20 14:15:47 +0200

    folder existance check

--- control/reco_track.sh
+++ control/reco_track.sh
@@ -3,4 +3,10 @@ cp -v ../code/reco/reco_track.C .;
 root -b reco_track.C+;
 rm *.d *.so *.pcm;
 mv reco_track.C scripts_done/;
+if [ -d "all/" ]; then
+  mv *png all/;
+else
+  mkdir all;
+  mv *png all/;
+fi
 cd ../;
